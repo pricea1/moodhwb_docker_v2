564fd9579ad53eabbcc2ccec9c63fbdea743d61d
Andrew Price <andrew@madebymoon.com>
2020-02-13 10:09:08 +0000

    Emoji support

--- site/config/db.php
+++ site/config/db.php
@@ -16,5 +16,6 @@ return [
     'database' => getenv('DB_DATABASE'),
     'schema' => getenv('DB_SCHEMA'),
     'tablePrefix' => getenv('DB_TABLE_PREFIX'),
-    'port' => getenv('DB_PORT')
+    'port' => getenv('DB_PORT'),
+    'charset' => 'utf8mb4'
 ];
